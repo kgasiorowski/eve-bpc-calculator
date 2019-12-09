6f35985d5dec334415fda6b1e1d16b0face5d279
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2019-12-09 03:14:40 -0500

    Added json files for invention

--- blueprint.py
+++ blueprint.py
@@ -1,49 +1,54 @@
-import market
 from market import Mode
 import os
 
+invention_path = './data/invention/'
 
 class Blueprint:
 
-    def __init__(self, _name, _input_items, _output_quant, _market, runs=1):
-        self.input_items = _input_items
-        self.name = _name
-        self.output_quant = _output_quant
-        self.runs = runs
-        self.market = _market
+    def __init__(self):
+        self.input_items = None
+        self.name = None
+        self.output_quant = None
+        self.runs = None
+        self.market = None
 
-    def get_input_costs(self, buyorders=True):
+        self.inventable = False
+        self.base_invention_chance = None
+        self.invented_runs = None
+        self.invented_TE = None
+        self.invented_ME = None
+        self.datacore1 = None
+        self.datacore2 = None
 
-        total_costs = 0
+    def get_market_results(self, buyorders=True, sellorders=True):
+
+        results = BlueprintMarketResults()
+
+        results.runs = self.runs
+        results.input_costs = 0
 
         for item_name, amount in self.input_items.items():
 
             buying_mode = Mode.BUYMAX if buyorders else Mode.SELLMIN
             item_price = self.market.get_market_attr_by_name(item_name, buying_mode)
-            total_costs += item_price * amount
-
-        return total_costs
-
-
-    def get_output_revenue(self, sellorders=True):
+            results.input_costs += item_price * amount
 
         selling_mode = Mode.SELLMIN if sellorders else Mode.BUYMAX
-        return self.market.get_market_attr_by_name(self.name, selling_mode) * self.output_quant
+        results.revenue = self.market.get_market_attr_by_name(self.name, selling_mode) * self.output_quant
 
+        results.profit = results.revenue - results.input_costs
+        results.total_profit = results.profit * results.runs
 
+        results.profit_margin = results.profit / results.input_costs
 
-    def get_profit_per_run(self, sellorders=True, buyorders=True):
-        return self.get_output_revenue(sellorders) - self.get_input_costs(buyorders)
-
-
-    def get_total_profit(self, sellorders=True, buyorders=True):
-        return self.get_profit_per_run(sellorders, buyorders)*self.runs
+        return results
 
 
     @staticmethod
-    def parse_string(str):
-        number = int(str[:str.find(' x ')].replace(',', '').strip())
-        item_name = str[str.rfind(' x ')+2:].strip()
+    def parse_string(string):
+
+        number = int(string[:string.find(' x ')].replace(',', '').strip())
+        item_name = string[string.rfind(' x ') + 2:].strip()
 
         return item_name, number
 
@@ -73,6 +78,37 @@ class Blueprint:
                     item_name, item_quantity = Blueprint.parse_string(line)
                     input_dict.setdefault(item_name, item_quantity)
 
-                blueprints.setdefault(outputname, Blueprint(outputname, input_dict, outputquantity, market_reference, num_runs))
+                blueprint = Blueprint()
+                blueprint.name = outputname
+                blueprint.input_items = input_dict
+                blueprint.output_quant = outputquantity
+                blueprint.market = market_reference
+                blueprint.runs = num_runs
+
+                try:
+                    with open(invention_path + filename) as invention_file:
+                        blueprint.inventable = True
+                        blueprint.base_invention_chance,\
+                        blueprint.invented_runs,\
+                        blueprint.invented_ME,\
+                        blueprint.invented_TE = next(invention_file).split()
+                        blueprint.datacore1 = next(invention_file)
+                        blueprint.datacore2 = next(invention_file)
+                except FileNotFoundError:
+                    pass
+
+                blueprints.setdefault(outputname, blueprint)
 
         return blueprints
+
+
+class BlueprintMarketResults:
+
+    def __init__(self):
+
+        self.runs = None
+        self.input_costs = None
+        self.revenue = None
+        self.profit_per_run = None
+        self.total_profit = None
+        self.profit_margin = None
