a367bd3d0440e7269342bfe36ec2a991156d26b1
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2020-07-27 23:25:34 -0400

    Added config file for more centralized paths

---
+++ src/config.py
@@ -0,0 +1,13 @@
+GENERATED_PATH = './generated'
+DATA_PATH = './data'
+SRC_PATH = './src'
+
+RAW_XLS_DATA = DATA_PATH + '/invTypes.xls'
+CONVERTED_CSV_DATA = GENERATED_PATH + '/invTypes.csv'
+
+DICT_PATH = GENERATED_PATH + '/dicts'
+ID_TO_NAME = DICT_PATH + '/id_to_name.json'
+NAME_TO_ID = DICT_PATH + '/name_to_id.json'
+
+CACHE_PATH = GENERATED_PATH + '/cache'
+MARKET_CACHE_PATH = CACHE_PATH + '/market_cache.json'
